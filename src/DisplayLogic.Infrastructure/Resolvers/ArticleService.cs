ab7862ef271ccfc96afecbead407642362d93366
Krzysiek Sobieraj <krzysiek@sobieraj.mobi>
2023-04-20 22:31:36 +0200

    feat(#11): change int to Guid for Author

--- src/DisplayLogic.Infrastructure/Resolvers/ArticleService.cs
+++ src/DisplayLogic.Infrastructure/Resolvers/ArticleService.cs
@@ -13,7 +13,7 @@ public class ArticleService : IArticleService
             Uuid = Guid.Parse("3d5d4cd1-b6f4-4ae4-a25a-918e185d6285"),
             Title = "Exploring the Flavors of Thai Cuisine",
             Content = "Thai cuisine is known for its bold flavors...",
-            Author = new Author { Id = 1, Username = "john_doe" },
+            Author = new Author { Id = Guid.Parse("e97ffece-bca2-4f06-84f9-a2d792afea20"), Username = "john_doe" },
             PublishedDate = new DateTime(2023, 4, 10),
             ImageUrl = "https://example.com/images/thai-cuisine.jpg",
             Tags = new List<Tag>
@@ -27,14 +27,14 @@ public class ArticleService : IArticleService
                 {
                     Id = 1,
                     Content = "Great article! I love Thai food.",
-                    Author = new Author { Id = 2, Username = "jane_doe" },
+                    Author = new Author { Id = Guid.Parse("f7cbdcf2-6291-42af-89f2-82e2dc3380bd"), Username = "jane_doe" },
                     CreatedAt = new DateTime(2023, 4, 11)
                 },
                 new Comment
                 {
                     Id = 2,
                     Content = "Thai cuisine is amazing!",
-                    Author = new Author { Id = 3, Username = "mark_smith" },
+                    Author = new Author { Id = Guid.Parse("164127d7-c6f7-4481-9671-1b986a462833"), Username = "mark_smith" },
                     CreatedAt = new DateTime(2023, 4, 12)
                 }
             }
@@ -44,7 +44,7 @@ public class ArticleService : IArticleService
             Uuid = Guid.Parse("34507ff9-6b73-4bae-98c3-af2ce2668188"),
             Title = "The Magic of Italian Pasta",
             Content = "Italian pasta dishes are a staple in many households...",
-            Author = new Author { Id = 2, Username = "jane_doe" },
+            Author = new Author { Id = Guid.Parse("689cffbf-2c88-4cb0-abce-f6bea1b28f3b"), Username = "jane_doe" },
             PublishedDate = new DateTime(2023, 4, 8),
             ImageUrl = "https://example.com/images/italian-pasta.jpg",
             Tags = new List<Tag>
@@ -58,14 +58,14 @@ public class ArticleService : IArticleService
                 {
                     Id = 3,
                     Content = "I can't wait to try some of these pasta dishes!",
-                    Author = new Author { Id = 1, Username = "john_doe" },
+                    Author = new Author { Id = Guid.Parse("e97ffece-bca2-4f06-84f9-a2d792afea20"), Username = "john_doe" },
                     CreatedAt = new DateTime(2023, 4, 9)
                 },
                 new Comment
                 {
                     Id = 4,
                     Content = "The article is very informative!",
-                    Author = new Author { Id = 3, Username = "mark_smith" },
+                    Author = new Author { Id = Guid.Parse("164127d7-c6f7-4481-9671-1b986a462833"), Username = "mark_smith" },
                     CreatedAt = new DateTime(2023, 4, 10)
                 }
             }
--- src/DisplayLogic.Infrastructure/Resolvers/CommentsService.cs
+++ src/DisplayLogic.Infrastructure/Resolvers/CommentsService.cs
@@ -16,7 +16,7 @@ public class CommentService : ICommentService
                 {
                     Id = 1,
                     Content = "First sample comment.",
-                    Author = new Author { Id = 1, Username = "author1" },
+                    Author = new Author { Id = Guid.Parse("4cc0ef2c-1982-4617-ab89-ff39dd92171e"), Username = "author1" },
                     CreatedAt = DateTime.Now.AddMinutes(-5),
                     ArticleId = Guid.Parse("3d5d4cd1-b6f4-4ae4-a25a-918e185d6285")
                 },
@@ -24,7 +24,7 @@ public class CommentService : ICommentService
                 {
                     Id = 2,
                     Content = "Second sample comment.",
-                    Author = new Author { Id = 2, Username = "author2" },
+                    Author = new Author { Id = Guid.Parse("da1ad829-2a0e-4bae-81d6-8ad30c4975f1"), Username = "author2" },
                     CreatedAt = DateTime.Now.AddMinutes(-10),
                     // ArticleId = Guid.Parse("3d5d4cd1-b6f4-4ae4-a25a-918e185d6285")
                     RecipeId = Guid.Parse("3d5d4cd1-b6f4-4ae4-a25a-918e185d6285")
@@ -33,7 +33,7 @@ public class CommentService : ICommentService
                 {
                     Id = 3,
                     Content = "Third sample comment.",
-                    Author = new Author { Id = 3, Username = "author3" },
+                    Author = new Author { Id = Guid.Parse("4210bd07-5f44-4d8a-9d97-6ee06b68d6e9"), Username = "author3" },
                     CreatedAt = DateTime.Now.AddMinutes(-15),
                     ArticleId = Guid.Parse("34507ff9-6b73-4bae-98c3-af2ce2668188")
                 },
@@ -41,7 +41,7 @@ public class CommentService : ICommentService
                 {
                     Id = 4,
                     Content = "Fourth sample comment.",
-                    Author = new Author { Id = 4, Username = "author4" },
+                    Author = new Author { Id = Guid.Parse("32e7bbc7-06f4-4997-a191-2d0691006edb"), Username = "author4" },
                     CreatedAt = DateTime.Now.AddMinutes(-20),
                     ArticleId = Guid.Parse("34507ff9-6b73-4bae-98c3-af2ce2668188")
                 },
@@ -49,7 +49,7 @@ public class CommentService : ICommentService
                 {
                     Id = 5,
                     Content = "Fifth sample comment.",
-                    Author = new Author { Id = 5, Username = "author5" },
+                    Author = new Author { Id = Guid.Parse("827324cd-b206-422f-bf86-26f7f83dc2ab"), Username = "author5" },
                     CreatedAt = DateTime.Now.AddMinutes(-25),
                     // ArticleId = Guid.Parse("34507ff9-6b73-4bae-98c3-af2ce2668188")
                     RecipeId = Guid.Parse("34507ff9-6b73-4bae-98c3-af2ce2668188")
